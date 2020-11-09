45a07ee6ffc2d84d2aa5518dc8de81fd929350d4
Rohan Juneja <rjuneja3@my.centennialcollege.ca>
2020-11-09 18:59:08 -0500

    Added  Options screen

---
+++ Assets/Scripts/OptionsMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class OptionsMenuController : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("GameVolume", 0.75f);
+    }
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("GameVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
