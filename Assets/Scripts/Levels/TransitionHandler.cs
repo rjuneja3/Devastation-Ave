266f6048efe25ad0ce242dd9f14d15532942bc9c
brenton <>
2020-12-08 11:50:09 -0500

    Changed scene build order

--- Assets/Scripts/Levels/TransitionHandler.cs
+++ Assets/Scripts/Levels/TransitionHandler.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Levels {
-    
-    // [RequireComponent(typeof(Collider))]
+    /**
+     * @author Brenton Hauth
+     * @date 12/07/20
+     * <summary>
+     * Handles Transitions between scenes
+     * </summary>
+     */
     public class TransitionHandler : MonoBehaviour {
-   
-        public static void LoadLevel()
-        {
+
+        /**
+         * @author Brenton Hauth
+         * @date 12/07/20
+         * <summary>Loads next level from based on level</summary>
+         */
+        public static void LoadNextLevel() {
+            // IMPORTANT: Can cause bug if scenes are not in order
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+            // May be better to do something like this instead:
+            //   string name = SceneManager.GetActiveScene().name;
+            //   name = $"Level {(char)(name[name.Length - 1] + 1)}";
+            //   SceneManager.LoadScene(name);
         }
 
         private void OnTriggerEnter(Collider c) => Collide(c.gameObject);
         private void OnCollisionEnter(Collision c) => Collide(c.gameObject);
 
+        /**
+         * @author Brenton Hauth
+         * @date 12/07/20
+         * <summary>
+         * Handles all collisions from Colliders
+         * </summary>
+         */
         private void Collide(GameObject o) {
             if (o.tag == "Player") {
-                // Trigger fade out
-                LoadLevel();
+                // TODO: Trigger fade out
+                LoadNextLevel();
             }
         }
     }
