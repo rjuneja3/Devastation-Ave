b3959bd22eed64eee377f050ce3631a55e8b8f93
Dinesh <dineshbalakrishnan12@gmail.com>
2020-12-13 18:53:12 -0500

    Added Script Unlockdoor, Added Enemies to level 2, Added TriggerExit

---
+++ Assets/UnlockDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockDoor : MonoBehaviour
+{
+    public GameObject GameObject;
+
+    private bool isDestroyed = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.GameObject == false)
+        {
+            isDestroyed = true;
+        }
+        if (isDestroyed == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
